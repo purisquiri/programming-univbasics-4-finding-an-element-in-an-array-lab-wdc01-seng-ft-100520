58e831dcf6ed08569a0b57b195990b6f74a72253
Mauro Dorigo <mau.dorigo@outlook.es>
2020-08-24 14:37:07 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   counter = 0
-  while counter < array.length 
+  while counter < array.length do
     value_to_find === array[]
     return array[]
   else nil
