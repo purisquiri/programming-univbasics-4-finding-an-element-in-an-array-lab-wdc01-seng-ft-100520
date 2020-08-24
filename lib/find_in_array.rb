9a56cad5050ce7f7c26bfdf3f8a480da7aa8442b
Mauro Dorigo <mau.dorigo@outlook.es>
2020-08-24 14:51:35 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,8 @@
 def find_element_index(array, value_to_find)
   counter = 0
+  found_value_index = nil
   while counter < array.length do
-    puts array[counter] == value_to_find
+    if array[counter] == value_to_find
     found_value_index = counter
     counter += 1
   end
